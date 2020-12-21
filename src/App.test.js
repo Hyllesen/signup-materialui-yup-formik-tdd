5f448d48f3fc4b523e7869451f4eadc5b562db39
Hyllesen <stefanhyltoft@gmail.com>
2020-12-21 07:53:00 +0100

    Form submit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,39 @@
 import { getByTestId, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
 describe("Sign up component", () => {
-  test("Renders all fields", () => {
+  let signUpBtn,
+    firstName,
+    lastName,
+    emailAddress,
+    password,
+    acceptTerms,
+    alreadyAccountLink;
+
+  beforeEach(() => {
     render(<App />);
+
     const signUpElements = screen.getAllByText(/sign up/i);
-    const signUpBtn = signUpElements[1];
-    const firstName = screen.getByText("First Name");
-    const lastName = screen.getByText("Last Name");
-    const emailAddress = screen.getByText("Email Address");
-    const password = screen.getByText("Password");
-    const acceptTerms = screen.getByText("I accept the terms and conditions");
-    const alreadyAccountLink = screen.getByText(
-      "Already have an account? Sign in"
+    signUpBtn = signUpElements[1];
+    firstName = screen.getByText("First Name");
+    lastName = screen.getByText("Last Name");
+    emailAddress = screen.getByText("Email Address");
+    password = screen.getByText("Password");
+    acceptTerms = screen.getByText("I accept the terms and conditions");
+    alreadyAccountLink = screen.getByText("Already have an account? Sign in");
+  });
+
+  test("Fill out form and submit", () => {
+    userEvent.type(firstName, "Stefan");
+    userEvent.type(lastName, "Hyltoft");
+    userEvent.type(emailAddress, "stefanhyltoft@©gmail.com");
+    userEvent.type(password, "myPassw0rt");
+    userEvent.click(acceptTerms);
+    userEvent.click(signUpBtn);
+
+    const confirmation = screen.getByText(
+      "Thanks for signing up! Please check your email for activation link."
     );
   });
 });
